4fdca5547c51918aac4b9152a33616e20464646a
Shruti P. Sangvikar <shrutisangvikar627@gmail.com>
2025-12-04 12:32:47 +0530

    Delete frontend directory

--- frontend/src/helpers/auth.js
+++
@@ -1,8 +0,0 @@
-export const isLoggedIn = () => {
-  return !!localStorage.getItem("token");
-};
-
-export const logout = () => {
-  localStorage.removeItem("token");
-  window.location.href = "/login";
-};
