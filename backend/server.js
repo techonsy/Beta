83a5f2032a457feb2970b0df5fd6707b89b111a2
Shruti627 <shrutisangvikar627@gmail.com>
2025-12-05 15:22:45 +0530

    frontend url

--- backend/server.js
+++ backend/server.js
@@ -13,12 +13,15 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://yourfrontend.vercel.app"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 
 connectDB();
 //remove comment to create superadmin
